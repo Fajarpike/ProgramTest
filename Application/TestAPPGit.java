f0261a51dc51f676139f43f0ab3730946187eb33
FajarPike <fpikekah@gmail.com>
2023-09-28 14:41:39 +0700

    menambahkan switch

--- Application/TestAPPGit.java
+++ Application/TestAPPGit.java
@@ -3,6 +3,26 @@ package Application;
 public class TestAPPGit {
     public static void main(String[] args) {
         System.out.println("Menambahkan satu kalimat");
+
+        String ucapan;
+
+        String nilai = "A";
+
+        
+    switch (nilai) {
+        case "A":
+          System.out.println("Wow, Anda Lulus Dengan Baik");
+        break;
+        case "B":
+            System.out.println("Nilai Anda Cukup Baik");
+        break;
+        case "C":
+            System.out.println("Anda Tidak Lulus");
+        break;
+        default: 
+          System.out.println("Muungkin Anda Salah Jurusan");
+        
+      }
     }
 }
 
